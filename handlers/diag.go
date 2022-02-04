f1f965e5e60d30e90d960494c0fb3a2978b5e538
Vegard Færgestad <vegarfae@stud.ntnu.no>
2022-02-04 23:54:51 +0100

    Diag now working as intended

--- handlers/diag.go
+++ handlers/diag.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
-	university_search "assignment-1"
+	"assignment-1/constants"
+	"assignment-1/uptime"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,37 +12,58 @@ func HandlerDiag(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method is not supported. Currently only GET are supported.", http.StatusMethodNotAllowed)
+		return
 	}
 
 	diagnose := getDiagnose()
 
-	_, err := fmt.Fprintf(w, "%v", output)
+	w.Header().Add("content-type", "application/json")
+
+	encoder := json.NewEncoder(w)
+
+	err := encoder.Encode(diagnose)
 	if err != nil {
-		http.Error(w, "Error when returning output", http.StatusInternalServerError)
+		http.Error(w, "Error during encoding", http.StatusInternalServerError)
+		return
 	}
 
 }
 
 func getDiagnose() Diagnose {
 
-	url := university_search.UNIVERSITIESAPI_URL
-	universityApiRequest, err := http.NewRequest(http.MethodGet, url, nil)
+	url := constants.UNIVERSITIESAPI_URL
+	universityApiRequest, err := http.NewRequest(http.MethodHead, url, nil)
 	if err != nil {
 		fmt.Errorf("Error in creating university API request: %e", err.Error())
 	}
 	universityApiRequest.Header.Add("content-type", "application/json")
 
-	url = university_search.COUNTRIESAPI_URL
+	url = constants.COUNTRIESAPI_URL
 	countriesApiRequest, err := http.NewRequest(http.MethodHead, url, nil)
 	if err != nil {
 		fmt.Errorf("Error in creating country API request: %e", err.Error())
 	}
 
 	client := http.Client{}
-	university, err := client.Do(universityApiRequest)
+	res, err := client.Do(universityApiRequest)
 	if err != nil {
 		fmt.Errorf("Error in university API response: %e", err.Error())
 	}
 
 	universityApiStatus := res.StatusCode
+
+	res, err = client.Do(countriesApiRequest)
+	if err != nil {
+		fmt.Errorf("Error in countries API response: %e", err.Error())
+	}
+
+	countriesApiStatus := res.StatusCode
+
+	return Diagnose{
+		UniversitiesApi: fmt.Sprintf("%d", universityApiStatus),
+		CountriesApi:    fmt.Sprintf("%d", countriesApiStatus),
+		Version:         "v1",
+		Uptime:          uptime.GetUptime(),
+	}
+
 }
