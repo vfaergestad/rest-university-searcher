fd08949dcb777dd45fa16e727fe41972de7e2f19
vegarfae <vegarfae@stud.ntnu.no>
2022-02-05 16:31:33 +0100

    Started developing uniinfo endpoint

--- handlers/structs.go
+++ handlers/structs.go
@@ -1,8 +1,24 @@
 package handlers
 
+import "go/types"
+
 type Diagnose struct {
 	UniversitiesApi string `json:"universitiesapi"`
 	CountriesApi    string `json:"countriesapi"`
 	Version         string `json:"version"`
 	Uptime          int    `json:"uptime"`
+}
+
+type Language struct {
+	Abbreviation string
+	Name         string
+}
+
+type University struct {
+	Name      string      `json:"name"`
+	Country   string      `json:"country"`
+	Isocode   string      `json:"isocode"`
+	Webpages  types.Array `json:"webpages"`
+	Languages Language    `json:"languages"`
+	Map       string      `json:"map"`
 }
--- handlers/uniinfo.go
+++ handlers/uniinfo.go
@@ -1,10 +1,48 @@
 package handlers
 
-import "net/http"
+import (
+	"assignment-1/constants"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path"
+)
 
 func HandlerUniInfo(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method is not supported. Currently only GET are supported.", http.StatusMethodNotAllowed)
 	}
+
+	requestUniInfo(r)
+
+}
+
+func requestUniInfo(r *http.Request) []University {
+
+	search := path.Base(r.URL.Path)
+	query := "search?name=" + search
+	url := constants.UNIVERSITIESAPI_URL + query
+
+	r, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Errorf("Error in creating University request: %e", err.Error())
+	}
+
+	r.Header.Add("content-type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		fmt.Errorf("Error in university response: %e", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		fmt.Errorf("Status code returned from universityAPI: %d", res.StatusCode)
+	}
+
+	decoder := json.NewDecoder(res.Body)
+
+	return
+
+	//url := constants.UNIVERSITIESAPI_URL + "search"
 
 }
