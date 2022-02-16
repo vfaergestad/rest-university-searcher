58e4d004907a056b71af0b358908d6d418e21c66
vegarfae <vegarfae@stud.ntnu.no>
2022-02-16 16:16:57 +0100

    Implemented limit query

--- handlers/neighbourunis.go
+++ handlers/neighbourunis.go
@@ -7,22 +7,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 func HandlerNeighbourUnis(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method is not supported. Currently only GET are supported.", http.StatusMethodNotAllowed)
+		return
 	}
 
 	pathList := strings.Split(r.URL.Path, "/")
 	countryQuery := pathList[len(pathList)-2]
 	uniQuery := pathList[len(pathList)-1]
+	var limit int
+	if l, err := strconv.Atoi(r.URL.Query()["limit"][0]); err != nil || l < 0 {
+		http.Error(w, "The limit is not a valid positive integer. Using 0 as limit.", http.StatusBadRequest)
+		limit = 0
+	} else {
+		limit = l
+	}
 
 	var countries []structs.Country
 	country := requests.RequestCountryInfo(fmt.Sprintf("%sname/%s?borders", constants.COUNTRIESAPI_URL, countryQuery))
 	for _, bor := range country.Borders {
-		url := fmt.Sprintf("%salpha/%s?fields=name,languange,maps", constants.COUNTRIESAPI_URL, bor)
+		url := fmt.Sprintf("%salpha/%s?fields=name,languages,maps", constants.COUNTRIESAPI_URL, bor)
 		countries = append(countries, requests.RequestCountryInfo(url))
 	}
 
@@ -32,11 +41,19 @@ func HandlerNeighbourUnis(w http.ResponseWriter, r *http.Request) {
 		url := constants.UNIVERSITIESAPI_URL + query
 		universities := requests.RequestUniInfo(url)
 		for _, u := range universities {
-			uniInfo = append(uniInfo, structs.CombineUniCountry(u, c))
+			if limit == 0 || len(uniInfo) < limit {
+				uniInfo = append(uniInfo, structs.CombineUniCountry(u, c))
+			} else {
+				break
+			}
+		}
+		if len(uniInfo) >= limit {
+			break
 		}
 	}
-	if uniInfo == nil {
+	if len(uniInfo) == 0 {
 		http.Error(w, "No universities found", http.StatusNoContent)
+		return
 	}
 	w.Header().Add("content-type", "application/json")
 	encoder := json.NewEncoder(w)
