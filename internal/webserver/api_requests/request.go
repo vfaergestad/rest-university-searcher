4992f5cc1cd57eb845af95f442bf971db3ff829d
Vegard Færgestad <vegarfae@stud.ntnu.no>
2022-03-14 23:08:24 +0100

    Created general request package

--- internal/webserver/api_requests/request.go
+++ internal/webserver/api_requests/request.go
@@ -1,1 +1,36 @@
 package api_requests
+
+import (
+	"assignment-2/internal/constants"
+	"errors"
+	"net/http"
+)
+
+func DoRequest(url string, method string) (*http.Response, error) {
+	switch method {
+	case
+		http.MethodHead,
+		http.MethodGet,
+		http.MethodDelete,
+		http.MethodConnect,
+		http.MethodOptions,
+		http.MethodPatch,
+		http.MethodPost,
+		http.MethodTrace:
+
+		r, err := http.NewRequest(method, url, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		client := &http.Client{}
+		res, err := client.Do(r)
+		if err != nil {
+			return nil, err
+		}
+
+		return res, nil
+	}
+	return nil, errors.New(constants.INVALID_METHOD_ERROR)
+
+}
