a0b6fc3a36a48c3684f31b8a33fd40b4a4e195ac
vegarfae <vegarfae@stud.ntnu.no>
2022-03-25 16:40:13 +0100

    Removed unused function

--- internal/webserver/utility/endcode_decode.go
+++ internal/webserver/utility/endcode_decode.go
@@ -5,14 +5,6 @@ import (
 	"net/http"
 )
 
-func DecodeResponse(res *http.Response, target interface{}) (interface{}, error) {
-	decoder := json.NewDecoder(res.Body)
-	if err := decoder.Decode(&target); err != nil {
-		return nil, err
-	}
-	return target, nil
-}
-
 func EncodeStruct(w http.ResponseWriter, target interface{}) error {
 	w.Header().Add("content-type", "application/json")
 	encoder := json.NewEncoder(w)
