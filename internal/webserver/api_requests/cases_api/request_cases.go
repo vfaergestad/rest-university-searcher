0279b68c1e4d04023a93731b3888f807a6993690
vegarfae <vegarfae@stud.ntnu.no>
2022-03-25 16:21:23 +0100

    Moved internal structs to its own file in the package

--- internal/webserver/api_requests/cases_api/request_cases.go
+++ internal/webserver/api_requests/cases_api/request_cases.go
@@ -15,31 +15,6 @@ const (
 	casesApiUrl = "https://covid19-graphql.now.sh"
 )
 
-type queryStruct struct {
-	Query string `json:"query"`
-}
-
-type casesApiResponse struct {
-	Data data `json:"data"`
-}
-
-type data struct {
-	Country countryStruct `json:"country"`
-}
-
-type countryStruct struct {
-	Name       string           `json:"name"`
-	MostRecent mostRecentStruct `json:"mostRecent"`
-}
-
-type mostRecentStruct struct {
-	Date       string  `json:"date"`
-	Confirmed  int     `json:"confirmed"`
-	Recovered  int     `json:"recovered"`
-	Deaths     int     `json:"deaths"`
-	GrowthRate float64 `json:"growthRate"`
-}
-
 func GetStatusCode() (int, error) {
 	res, err := getResponse("Norway")
 	if err != nil {
