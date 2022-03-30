e5bc88e0fc5d6511386b6d933f4c5fe1ab8d1eb7
vegarfae <vegarfae@stud.ntnu.no>
2022-03-30 23:37:37 +0200

    Refactored imports

--- internal/webserver/init.go
+++ internal/webserver/init.go
@@ -4,7 +4,11 @@ import (
 	"assignment-2/internal/webserver/cache/country_cache"
 	"assignment-2/internal/webserver/constants"
 	"assignment-2/internal/webserver/db"
-	"assignment-2/internal/webserver/handlers"
+	"assignment-2/internal/webserver/handlers/cases_handler"
+	"assignment-2/internal/webserver/handlers/default_handler"
+	"assignment-2/internal/webserver/handlers/notifications_handler"
+	"assignment-2/internal/webserver/handlers/policy_handler"
+	"assignment-2/internal/webserver/handlers/status_handler"
 	"assignment-2/internal/webserver/utility/uptime"
 	"log"
 	"net/http"
@@ -34,11 +38,11 @@ func InitServer() {
 	}
 
 	// Points the different URL-paths to the correct handler
-	http.HandleFunc(constants.DefaultPath, handlers.HandlerDefault)
-	http.HandleFunc(constants.CasesPath, handlers.HandlerCases)
-	http.HandleFunc(constants.PolicyPath, handlers.HandlerPolicy)
-	http.HandleFunc(constants.StatusPath, handlers.HandlerStatus)
-	http.HandleFunc(constants.NotificationsPath, handlers.HandlerNotifications)
+	http.HandleFunc(constants.DefaultPath, default_handler.HandlerDefault)
+	http.HandleFunc(constants.CasesPath, cases_handler.HandlerCases)
+	http.HandleFunc(constants.PolicyPath, policy_handler.HandlerPolicy)
+	http.HandleFunc(constants.StatusPath, status_handler.HandlerStatus)
+	http.HandleFunc(constants.NotificationsPath, notifications_handler.HandlerNotifications)
 
 	// Starting HTTP-server
 	log.Println("Starting server on port " + defaultPort + " ...")
